8414597a09f777e556badaddf0dadc9ae794e344
alessio iafrate <alessioiafrate@hotmail.com>
2023-10-26 18:50:49 +0200

    maui blazor hybrid project

---
+++ MauiHybrid/CustomNavigationManager.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiHybrid
+{
+    public class CustomNavigationManager
+    {
+        private static NavigationManager _navigationManager;
+        public CustomNavigationManager(NavigationManager MyNavigationManager)
+        {
+            _navigationManager = MyNavigationManager;
+        }
+
+        public static void Navigation(string url)
+        {
+            if (_navigationManager != null)
+            {
+                _navigationManager.NavigateTo(url);
+            }
+        }
+    }
+}
---
+++ MauiHybrid/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace MauiHybrid
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            
+            blazorWebView.
+        }
+    }
+}
